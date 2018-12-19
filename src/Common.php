b1a203f5ae1861cdc8796c57b49432f284092dec
buffge <1515888956@qq.com>
2018-12-19 14:16:27 +0800

    ali 短信

---
+++ src/Common.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Author: buff <admin@buffge.com>
+ * Created on : 2018-12-19, 14:12:54
+ * QQ:1515888956
+ */
+namespace bdk;
+
+abstract class Common
+{
+    public static function isMobilePhone(string $phone): bool
+    {
+        return preg_match("~1[356789]\d{9}~", $phone) === 1;
+    }
+}
